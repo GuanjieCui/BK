dd9b0640d674bdeafbeef3494da764a4f67611f7
figotj <leitao8708@gmail.com>
2019-03-30 12:24:03 -0500

    Update info.go

--- pegasus/utils/info.go
+++ pegasus/utils/info.go
@@ -13,3 +13,23 @@ type User struct {
 type Item struct {
 
 }
+
+type Order struct {
+	UserID     string  `json:"user_id"`
+	Size       string  `json:"size"`
+	Weight     float64 `json:"weight"`
+	Arrival    string  `json:"arrival"`
+	PickupLoc  string  `json:"pickup"`
+	DropoffLoc string  `json:"dropoff"`
+}
+
+type DeliverMethod struct {
+	RobotTime     float64 `json:"robot_time"`
+	RobotDistance float64 `json:"robot_distance"`
+	RobotPrice    float64 `json:"robot_price"`
+	DroneTime     float64 `json:"drone_time"`
+	DroneDistance float64 `json:"drone_distance"`
+	DronePrice    float64 `json:"drone_price"`
+	FormatPickup  string  `json:"pickup"`
+	FormatDropoff string  `json:"dropoff"`
+}
